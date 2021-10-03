7d368f3b1cf38eba29bdaccee34fcc36e34e528a
kevin7816 <kevinliang7816@gmail.com>
2021-10-03 16:27:45 -0700

    Update hlian050_lab2_part3_tests.py

--- Lab2_introToAVR/turnin/hlian050_lab2_part3_tests.py
+++ Lab2_introToAVR/turnin/hlian050_lab2_part3_tests.py
@@ -1,50 +1,86 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'PINA: 0x00 => PORTC: 0x04',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x04)],
+    },
+    {'description': 'PINA: 0x01 => PORTC: 0x03',
+    'steps': [ {'inputs': [('PINA', 0x01)],'iterations': 5}, ],
+    'expected': [('PORTC',0x03)],
+    },
+    {'description': 'PINA: 0x02 => PORTC: 0x03',
+    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x03)],
+    },
+    {'description': 'PINA: 0x03 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x04 => PORTC: 0x03',
+    'steps': [ {'inputs': [('PINA',0x04)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x03)],
+    },
+    {'description': 'PINA: 0x05 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x05)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x06 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x07 => PORTC: 0x01',
+    'steps': [ {'inputs': [('PINA',0x07)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x01)],
+    },
+    {'description': 'PINA: 0x08 => PORTC: 0x03',
+    'steps': [ {'inputs': [('PINA',0x08)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x03)],
+    },
+    {'description': 'PINA: 0x09 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x09)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x0A => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x0A)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x0B => PORTC: 0x01',
+    'steps': [ {'inputs': [('PINA',0x0B)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x01)],
+    },
+    {'description': 'PINA: 0x0C => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x0C)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x0D => PORTC: 0x01',
+    'steps': [ {'inputs': [('PINA',0x0D)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x01)],
+    },
+    {'description': 'PINA: 0x0E => PORTC: 0x01',
+    'steps': [ {'inputs': [('PINA',0x0E)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x01)],
+    },
+    {'description': 'PINA: 0x0F => PORTC: 0x80',
+    'steps': [ {'inputs': [('PINA',0x0F)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x80)],
+    },
+    ]
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0xFF; PORTB = 0x00;
-    /* Insert your solution below */
-    unsigned char spot1 = 0x00;
-    unsigned char spot2 = 0x00;
-    unsigned char spot3 = 0x00;
-    unsigned char spot4 = 0x00;
-    unsigned char cntavail;
-    while (1) {
-	cntavail = 0x04;
-	spot1 = PINA & 0x01;
-	spot2 = PINA & 0x02;
-	spot3 = PINA & 0x04;
-	spot4 = PINA & 0x08;
-
-	if(spot1 == 0x01){
-	cntavail--;
-	}
-	if(spot2 ==0x02){
-	cntavail--;
-	}
-	if(spot3 == 0x04){
-	cntavail--;
-	}
-	if(spot4 == 0x08){
-	cntavail--;
-	}
-	if (cntavail == 0){
-	cntavail = 0x80;
-	}
-	PORTC = cntavail;
-    }
-    return 0;
-}
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+watch = ['PORTB']
