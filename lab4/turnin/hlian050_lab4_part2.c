116ae96a768b824515dd7345a9854e487499ec37
Ubuntu <kevinliang7816@gmail.com>
2021-10-10 01:29:46 -0700

    adding file

---
+++ lab4/turnin/hlian050_lab4_part2.c
@@ -0,0 +1,74 @@
+/*	Author: Kevin Liang
+ *  Partner(s) Name: 
+ *	Lab Section: 23
+ *	Assignment: Lab #4  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void Tick(unsigned char input);
+
+enum states {START, ADD, SUB, RESET, CONTROL} state;
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA=0xFF;
+	DDRB = 0xFF; PORTB=0x00;
+	unsigned char input = 0x00;
+	state = START;
+	PORTC = 0x07;
+    /* Insert your solution below */
+    while (1) {
+	input = PINA;
+	Tick(input);
+    }
+    return 1;
+}
+
+void Tick(unsigned char input) {
+	switch(state){
+	case START:
+		PORTC = 0x07;
+		state = CONTROL;
+		break;
+	
+	case CONTROL:
+		if(input == 0x03){
+		state = RESET;
+		} else if(input == 0x01 ){
+		state = ADD;
+		} else if(input == 0x02 ){
+		state = SUB;
+		} else {
+		break;
+		}
+		break;
+
+	case ADD:
+		if(PORTC < 9){
+		PORTC++;
+		}
+		state = CONTROL;
+		break;
+
+	case SUB:
+		if(PORTC > 0){
+		PORTC--;
+		}
+		state = CONTROL;
+		break;
+
+	case RESET: 
+		PORTC = 0x00;
+		break;
+
+	default:
+		break;
+	}
+}
