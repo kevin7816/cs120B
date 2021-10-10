e6d88e6356ae0c7b260612fe68eaae950f8e1549
Ubuntu <kevinliang7816@gmail.com>
2021-10-10 03:38:10 -0700

    Completed part 4

---
+++ lab4/turnin/hlian050_lab4_part4.c
@@ -0,0 +1,81 @@
+/*	Author: Kevin Liang
+ *  Partner(s) Name: 
+ *	Lab Section: 23
+ *	Assignment: Lab #4  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void Tick(unsigned char input);
+
+enum states {START, X, Y, PRESS, RELEASED} state;
+unsigned char count1, count2, count3, count4 = 0x00;
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA=0xFF;
+	DDRB = 0xFF; PORTB=0x00;
+	unsigned char input,temp = 0x00;
+	state = START;
+    /* Insert your solution below */
+    while (1) {
+	input = PINA;
+	Tick(input);
+	temp = input & 0x80;
+	if(temp == 0x80){
+	PORTB = 0x00;
+	}
+    }
+    return 1;
+}
+
+void Tick(unsigned char input) {
+	switch(state){
+	case START:
+		state = PRESS;
+		break;
+	
+	case PRESS:
+		if(input == 0x04){
+		state = RELEASED;
+		} else {
+		state = PRESS;
+		}
+		break;
+
+	case RELEASED:
+		if(input == 0x00){
+		state = Y;
+		} else {
+		state = PRESS;
+		}
+		break;
+
+	case Y:
+		if(input == 0x02){
+		    if(PORTB == 0x00){
+			count1++;
+			PORTB = 0x01;
+		    } else {
+			count2++;
+		        PORTB = 0X00;
+		    }
+		count3++;
+		}
+		count4++;
+		state = PRESS;
+		break;
+
+	case X: 
+		state = PRESS;
+		break;
+
+	default:
+		break;
+	}
+}
