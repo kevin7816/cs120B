f75361ad9f4228a67fa2c340ec3c300e668b9071
kevin7816 <kevinliang7816@gmail.com>
2021-10-31 22:40:20 -0700

    Update hlian050_lab8_part1.c

--- lab8/turnin/hlian050_lab8_part1.c
+++ lab8/turnin/hlian050_lab8_part1.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Youtube Link: https://youtu.be/OBtXIIHhQz0
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
