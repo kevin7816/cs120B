2e5d962ef4cbfa144a15170c814ac0bf853bac42
Ubuntu <kevinliang7816@gmail.com>
2021-10-31 22:23:05 -0700

    Completed part 4

---
+++ lab8/turnin/hlian050_lab8_part4.c
@@ -0,0 +1,53 @@
+/*	Author: Kevin Liang
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab 8  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+unsigned char max = 0xFF;
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	unsigned char output, level = 0x00;
+	ADC_init();
+    /* Insert your solution below */
+    while (1) {
+	unsigned short myShort = ADC;
+	level = max/8;
+	if( myShort >= max){
+		output = 0xFF;
+	} else if (myShort >= (level * 7)){
+		output = 0x7F;
+	} else if (myShort >= (level * 6)){
+		output = 0x3F;
+	} else if (myShort >= (level * 5)){
+                output = 0x1F;
+        } else if (myShort >= (level * 4)){
+                output = 0x0F;
+        } else if (myShort >= (level * 3)){
+                output = 0x07;
+        } else if (myShort >= (level * 2)){
+                output = 0x03;
+        } else if (myShort >= level){
+                output = 0x01;
+        }
+	PORTB = output;
+    }
+    return 1;
+}
