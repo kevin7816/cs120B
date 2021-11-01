661f0d84a63d2b35f6017022f9b684c9d94fd52b
kevin7816 <kevinliang7816@gmail.com>
2021-10-31 23:33:25 -0700

    Update hlian050_lab9_part1.c

--- lab9/turnin/hlian050_lab9_part1.c
+++ lab9/turnin/hlian050_lab9_part1.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Youtube Link: https://youtu.be/hNTEvBVmsDs
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
