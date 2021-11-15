07ba4e632915293409d819b0893ece6c134d8392
Ubuntu <kevinliang7816@gmail.com>
2021-11-14 23:16:04 -0800

    Completed part1

---
+++ lab11/turnin/hlian050_lab11_part1.c
@@ -0,0 +1,135 @@
+/*	Author: Kevin Liang
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <timer.h>
+#include <keypad.h>
+#include "io.h"
+#include <scheduler.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned char output, temp = 0x00;
+
+enum keypadStates{START, WAIT};
+int KeypadTick(int state) {
+	temp = GetKeypadKey();
+	switch(state) {
+		case START:
+			state = WAIT; 
+			break;
+		case WAIT:
+			state = WAIT;	
+			break;
+		default:	
+			break;
+	}
+
+	switch(state) {
+		case START:	
+			break;
+		case WAIT:	
+			switch(temp) {
+				case '\0':
+				output = 0x1F;
+				break;
+				case '1':
+				output = 0x01;
+				break;
+				case '2':
+				output = 0x02;
+				break;
+				case '3':
+				output = 0x03;
+				break;
+				case '4':
+				output = 0x04;
+				break;
+				case '5':
+				output = 0x05;
+				break;
+				case '6':
+				output = 0x06;
+				break;
+				case '7':
+				output = 0x07;
+				break;
+				case '8':
+				output = 0x08;
+				break;
+				case '9':
+				output = 0x09;
+				break;
+				case 'A':
+				output = 0x0A;
+				break;
+				case 'B':
+				output = 0x0B;
+				break;
+				case 'C':
+				output = 0x0C;
+				break;
+				case 'D':
+				output = 0x0D;
+				break;
+				case '*':
+				output = 0x0E;
+				break;
+				case '0':
+				output = 0x00;
+				break;
+				case '#':
+				output = 0x0F;
+				break;
+				default:
+				output = 0x1B;
+				break;
+			}
+			PORTB = output;
+			break;
+				
+		default:
+			break;
+	}
+	return state;
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xF0; PORTC = 0x0F;
+    /* Insert your solution below */
+    static task task1;
+    task *tasks[] = { &task1};
+    const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
+    const char start = 0;
+
+    task1.state = start;
+    task1.period = 20;
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &KeypadTick;
+
+    TimerSet(20);
+    TimerOn();
+
+    unsigned short i;
+    while (1) {
+	for (i = 0; i < numTasks; ++i) {
+		if (tasks[i]->elapsedTime == tasks[i]->period) {
+			tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+			tasks[i]->elapsedTime = 0;
+		}
+		tasks[i]->elapsedTime += 20;
+	}
+	while(!TimerFlag);
+	TimerFlag = 0;
+    }
+    return 1;
+}
